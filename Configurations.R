cedfde389cfbfe8328ba277d7e373b70db4fca78
Ectelion <rustemb712@gmail.com>
2014-05-12 04:20:11 +0200

    Configurations file

---
+++ Configurations.R
@@ -0,0 +1,6 @@
+## Global Variables
+emotionLabelsFolder   <- "C:/Users/user/Documents/Docs_HP/TUM/Semester II/Data Mining Praktikum/FER Dataset/Data provided/CK+/Emotion_labels/Emotion"
+landmarksFolder       <- "C:/Users/user/Documents/Docs_HP/TUM/Semester II/Data Mining Praktikum/FER Dataset/Data provided/CK+/Landmarks/Landmarks"
+AULabelsFolder        <- "C:/Users/user/Documents/Docs_HP/TUM/Semester II/Data Mining Praktikum/FER Dataset/Data provided/CK+/FACS_labels/FACS"
+faceExpressionsFolder <- "C:/Users/user/Documents/Docs_HP/TUM/Semester II/Data Mining Praktikum/FER Dataset/W2. Feature Extraction/Face expressions"
+outputFolder          <- "Observations"
