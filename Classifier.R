53138064a98e2e872d916134bcc79c872d6ee5af
Ectelion <rustemb712@gmail.com>
2014-06-18 02:24:14 +0200

    Abstract classifier, providing a common interface to the concrete classifiers, including Decision Tree, Random Forest, Binary classifier, etc

---
+++ Classifier.R
@@ -0,0 +1,42 @@
+source("RandomForestClassifier.R")
+source("BinaryClassifier.R")
+source("DecisionTreeClassifier.R")
+
+
+## Specifies a common interface for a variety of classificator objects, containing 
+## functionality for training, prediction, cross validation, etc.
+## @parameter "type": {"decision_tree", "random_forest", binary", "svm", "neural_network", "naive_bayes"} 
+
+classifier <- function(data, type="decision_tree") {	
+	classifier <- NA
+	
+	if (type=="decision_tree") {
+		classifier <- initTree(data)
+	} 
+	else if (type=="random_forest") {
+		classifier <- initForest(data)
+	}
+	else if (type=="binary") {
+		classifier <- initBinClassifier(data)
+	}
+	
+	predict <- function(entry) {
+		classifier$predict(entry)
+	}
+	
+	test <- function(testSet) {
+		classifier$test(testSet)
+	}
+	
+	hitsNum <- function(testSet, trueLabels) {
+		classifier$hitsNum(testSet, trueLabels)
+	}
+	
+	# Static method, performing a K-foldes cross-validation  
+	crossValidation <- function(K=10) {
+		classifier$crossValidation(data, K)
+	}
+	
+	## Returns a list representation of the object with methods and properties accessed through indexed keys
+	list(classifier=classifier, predict=predict, crossValidation=crossValidation, test=test, hitsNum = hitsNum)
+}  
